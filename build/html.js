bdbcf38a1c78812832c1a3bd0f872f3a4c54af53
mikeg <mikeg@steelhouse.com>
2017-07-21 13:46:01 -0700

    mobile header enhancement

--- build/html.js
+++ build/html.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "build/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 459);
+/******/ 	return __webpack_require__(__webpack_require__.s = 461);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 459:
+/***/ 461:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
