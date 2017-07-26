8862bf94791df405ac8031461af23153fb8d96cf
mikeg <mikeg@steelhouse.com>
2017-07-26 16:13:19 -0700

    reusable project component

---
+++ src/components/projects/data/projects.js
@@ -0,0 +1,51 @@
+import projectOne from '../../../assets/images/mike.jpg';
+import projectTwo from '../../../assets/images/bg-mountains-mobile.jpg';
+import projectThree from '../../../assets/images/bg-code-mobile.jpg';
+
+export const projects = [
+	{
+		title: 'Project One',
+		stack: [
+			{ name: 'React', url: 'https://facebook.github.io/react/' },
+			{ name: 'Redux', url: 'http://redux.js.org/' },
+			{ name: 'Node', url: 'https://nodejs.org/' },
+			{ name: 'LESS', url: 'http://lesscss.org/' },
+			{ name: 'Webpack', url: 'https://webpack.github.io/' }
+		],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		image: projectOne,
+		url: 'http://google.com',
+		github: 'https://github.com/mikegutierrez'
+
+	},
+	{
+		title: 'Project Two',
+		stack: [
+			{ name: 'React', url: 'https://facebook.github.io/react/' },
+			{ name: 'Redux', url: 'http://redux.js.org/' },
+			{ name: 'Node', url: 'https://nodejs.org/' },
+			{ name: 'LESS', url: 'http://lesscss.org/' },
+			{ name: 'Webpack', url: 'https://webpack.github.io/' }
+		],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		image: projectTwo,
+		url: 'http://google.com',
+		github: 'https://github.com/mikegutierrez'
+
+	},
+	{
+		title: 'Project Three',
+		stack: [
+			{ name: 'React', url: 'https://facebook.github.io/react/' },
+			{ name: 'Redux', url: 'http://redux.js.org/' },
+			{ name: 'Node', url: 'https://nodejs.org/' },
+			{ name: 'LESS', url: 'http://lesscss.org/' },
+			{ name: 'Webpack', url: 'https://webpack.github.io/' }
+		],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		image: projectThree,
+		url: 'http://google.com',
+		github: 'https://github.com/mikegutierrez'
+
+	}
+];
--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -3,52 +3,57 @@ import React, { Component } from 'react';
 // Global components
 import SectionTitle from '../global/SectionTitle';
 
-import placeholder from '../../assets/images/mike.jpg';
+import { projects } from './data/projects';
 
 class Project extends Component {
-	render() {
+
+	renderProjects() {
 		return (
-			<div className="row margin-bottom-lg project section">
-				<div className="col-md-12">
-					<SectionTitle title="Project One" classes={['inline-block']}/>
-				</div>
-				<div className="col-md-4 col-sm-12 margin-bottom">
-					<img src={placeholder} width="220"/>
-				</div>
-				<div className="col-md-8 col-sm-12">
-					<div className="tags">
-						<a href="#" target="_blank">Node.js</a>
-						<a href="#" target="_blank">React</a>
-						<a href="#" target="_blank">Redux</a>
-						<a href="#" target="_blank">LESS</a>
-						<a href="#" target="_blank">Webpack</a>
-					</div>
-					<div className="row margin-top">
+			projects.map((project, index) => {
+				return (
+					<div className="row margin-bottom-lg project section" key={index}>
 						<div className="col-md-12">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							<SectionTitle title={project.title} classes={['inline-block']}/>
+						</div>
+						<div className="col-md-4 col-sm-12 margin-bottom">
+							<img src={project.image} width="220"/>
+						</div>
+						<div className="col-md-8 col-sm-12">
+							<div className="tags">
+								{
+								project.stack.map((tech, index) => {
+									return <a href={tech.url} target="_blank" key={index}>{tech.name}</a>;
+								})
+								}
+							</div>
+							<div className="row margin-top">
+								<div className="col-md-12">{project.description}</div>
+							</div>
+							<div className="row margin-top-m">
+								<div className="col-md-12">
+									<ul className="list-inline">
+										<li>
+											<a href={project.url} target="_blank" className="small margin-right-lg">
+												Visit the project &gt;&gt;
+											</a>
+										</li>
+										<li>
+											<a href={project.github} target="_blank" className="small">
+												View the code on Github &gt;&gt;
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div className="row margin-top-m">
-						<div className="col-md-12">
-							<ul className="list-inline">
-								<li>
-									<a href="#" target="_blank" className="small margin-right-lg">
-										Visit the project &gt;&gt;
-									</a>
-								</li>
-								<li>
-									<a href="#" target="_blank" className="small">
-										View the code on Github &gt;&gt;
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
+				);
+			})
 		);
+	}
+	render() {
+		const content = projects.length ? this.renderProjects() : {};
+		return <div>{ content }</div>;
 	}
 }
 
