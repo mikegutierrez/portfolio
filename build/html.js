337dd51d45602dcb6fe11dfb838408e8158ff0c5
mikeg <mikeg@steelhouse.com>
2017-08-04 13:47:41 -0700

    hook up 4 projects to route

--- build/html.js
+++ build/html.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "build/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 464);
+/******/ 	return __webpack_require__(__webpack_require__.s = 468);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 464:
+/***/ 468:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
